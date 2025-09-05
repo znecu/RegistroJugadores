31916ae33dbd4c00e8bad3d456e7818c31aa8bc2
znecu <alma_maria@ucne.edu.do>
2025-09-05 14:23:35 -0400

    Carga inicial de los archivos del proyecto de la tarea 1: RegistroJugadores

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+val Any.serialization: kotlin.Any
+
+val org.gradle.accessors.dm.LibrariesForLibs.kotlin: kotlin.Any
+
+val Any.json: kotlin.Any
+
+val org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension.serialization: kotlin.Any
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "edu.ucne.RegistroJugadores"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "edu.ucne.RegistroJugadores"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+}

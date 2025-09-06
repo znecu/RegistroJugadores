9e7fa895c7652e8a34832c5f2d9bf3d16bfe115a
Alma Maria <alma_maria@ucne.edu.do>
2025-09-06 15:12:10 -0400

    inyecté el constructor y agregué el metodo para la validación

--- app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/repository/JugadorRepositoryImpl.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/repository/JugadorRepositoryImpl.kt
@@ -7,28 +7,31 @@ import edu.ucne.RegistroJugadores.domain.jugadores.model.Jugador
 import edu.ucne.RegistroJugadores.domain.jugadores.repository.JugadorRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class JugadorRepositoryImpl(
+class JugadorRepositoryImpl @Inject constructor(
     private val dao: JugadorDao
-) : JugadorRepository{
+) : JugadorRepository {
 
     override fun observeJudador(): Flow<List<Jugador>> = dao.ObserveAll().map{
-        list -> list.map { it.toDomain() }
+            list -> list.map { it.toDomain() }
     }
+
     override suspend fun getJugador(id: Int?): Jugador? = dao.getById(id)?.toDomain()
 
     override suspend fun upsert(jugador: Jugador): Int{
         dao.upsert(jugador.toEntity())
-        return  jugador.jugadorId
+        return jugador.jugadorId
     }
 
     override suspend fun delete(judador: Jugador) {
         dao.delete(judador.toEntity())
-
     }
 
     override suspend fun deleteById(id: Int) {
         dao.deleteById(id)
     }
-
+    override suspend fun existeNombre(nombre: String, excludeId: Int?): Boolean {
+        return dao.existeNombre(nombre, excludeId)
+    }
 }

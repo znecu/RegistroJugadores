a09c4c7569539c635db14d0c749b34e1046c7bc3
znecu <alma_maria@ucne.edu.do>
2025-09-05 21:19:55 -0400

    inyectando la base de datos

---
+++ app/src/main/java/edu/ucne/RegistroJugadores/di/AppModule.kt
@@ -0,0 +1,29 @@
+package edu.ucne.RegistroJugadores.di
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.RegistroJugadores.data.jugadores.local.database.JugadorDb
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideJugadorDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            JugadorDb::class.java,
+            "Jugador.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideJugadorDao(jugadorDb: JugadorDb) = jugadorDb.jugadorDao()
+
+
+}

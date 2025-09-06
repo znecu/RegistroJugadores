9f0502f4acb53d79740103396dae297b6b13f0d1
Alma Maria <alma_maria@ucne.edu.do>
2025-09-06 14:24:31 -0400

    actualizando plugins, libs y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,26 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-
-//    id("androidx.room")
+//plugins {
+//    alias(libs.plugins.android.application)
+//    alias(libs.plugins.kotlin.android)
+//    alias(libs.plugins.kotlin.compose)
+//    alias(libs.plugins.kotlin.serialization)
 //    id("com.google.devtools.ksp")
 //    id("com.google.dagger.hilt.android")
-//    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlin.serialization)
-
+//}
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp") version "2.0.0-1.0.23"
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
-    namespace = "edu.ucne.tarea1"
+    namespace = "edu.ucne.RegistroJugadores"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "edu.ucne.tarea1"
+        applicationId = "edu.ucne.RegistroJugadores"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -34,19 +38,93 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
 }
 
+//dependencies {
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.lifecycle.runtime.ktx)
+//    implementation(libs.androidx.activity.compose)
+//    implementation(platform(libs.androidx.compose.bom))
+//    implementation(libs.androidx.compose.ui)
+//    implementation(libs.androidx.compose.ui.graphics)
+//    implementation(libs.androidx.compose.ui.tooling.preview)
+//    implementation(libs.androidx.compose.material3)
+//
+//    //Hilt
+//    implementation("com.google.dagger:hilt-android:2.51")
+//    ksp("com.google.dagger:hilt-compiler:2.51")
+//    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+//
+//    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
+//
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
+//    androidTestImplementation(platform(libs.androidx.compose.bom))
+//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+//    debugImplementation(libs.androidx.compose.ui.tooling)
+//    debugImplementation(libs.androidx.compose.ui.test.manifest)
+//
+//    // Navigation
+//    implementation(libs.androidx.navigation.compose)
+//    implementation(libs.kotlinx.serialization.json)
+//
+//    // Room
+//    implementation("androidx.room:room-runtime:2.6.1")
+//    implementation(libs.androidx.junit.ktx)
+//    implementation(libs.androidx.activity.ktx)
+//    annotationProcessor("androidx.room:room-compiler:2.6.1")
+//    ksp("androidx.room:room-compiler:2.6.1")
+//}
+
 dependencies {
+
+    //imagenes
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.activity.ktx)
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+
+    implementation("androidx.compose.foundation:foundation:1.5.0")
+
+    //optional
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+    implementation ("androidx.compose.material:material:1.4.0")
+    implementation("androidx.compose.material:material:1.3.1") // Usa la última versión
+    implementation("androidx.compose.material:material-icons-extended")
+
+
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.51")
+    ksp("com.google.dagger:hilt-android-compiler:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -57,96 +135,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    // Room
-    implementation(libs.androidx.room.runtime.v261)
-//    ksp("androidx.room:room-compiler:2.6.1")
-    implementation(libs.androidx.room.ktx.v261)
-
-    // ViewModel y LiveData
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
-
-    // Compose
-    implementation("androidx.compose.ui:ui:1.6.7")
-    implementation("androidx.compose.material3:material3:1.2.1")
-
-    val composeBom = platform("androidx.compose:compose-bom:2025.08.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-
-    // Material Design 3
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.foundation:foundation")
-
-    implementation("androidx.compose.ui:ui")
-
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-
-
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3.adaptive:adaptive")
-
-    implementation("androidx.activity:activity-compose:1.10.1")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
-
-    implementation("androidx.compose.runtime:runtime-livedata")
-
-    implementation("androidx.compose.runtime:runtime-rxjava2")
-
-    //
-
-
-
-
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
-
-    //navegacion
-    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
-//    implementation(libs.kotlin.serialization.json)
-
-    //room
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-//    ksp("androidx.room:room-compiler:2.6.1")
-    //  optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.6.1")
-
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.51")
-//    ksp("com.google.dagger:hilt-android-compiler:2.51")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
-    //retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    testImplementation(libs.junit)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("com.google.truth:truth:1.4.2")
-    testImplementation("io.mockk:mockk:1.13.13")
-    testImplementation("app.cash.turbine:turbine:1.1.0")
-    testImplementation("org.robolectric:robolectric:4.11.1")
-    testImplementation("com.google.truth:truth:1.4.2")
-    testImplementation("junit:junit:4.13.2")
-
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
-    alias(libs.plugins.kotlin.ksp) apply false
+//plugins {
+//        alias(libs.plugins.android.application) apply false
+//        alias(libs.plugins.kotlin.android) apply false
+//        alias(libs.plugins.kotlin.compose) apply false
+//        alias(libs.plugins.kotlin.serialization) apply false
+//        id("androidx.room") version "2.7.2" apply false
+//        alias(libs.plugins.ksp) apply false
+//        id("com.google.dagger.hilt.android") version "2.51" apply false
+//
+//}
 
+plugins {
+        alias(libs.plugins.android.application) apply false
+        alias(libs.plugins.jetbrains.kotlin.android) apply false
+        alias(libs.plugins.kotlinx.serialization) apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+        id("androidx.room") version "2.6.1" apply false
+        id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
+        id("com.google.dagger.hilt.android") version "2.51" apply false
 }

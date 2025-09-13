824ab5863e72bc5d3a0aa4b19dd3c7c628a758e5
Alma Maria <alma_maria@ucne.edu.do>
2025-09-13 00:23:10 -0400

    creando el partida dao

---
+++ app/src/main/java/edu/ucne/RegistroJugadores/partidas/local/PartidaDao.kt
@@ -0,0 +1,26 @@
+package edu.ucne.RegistroJugadores.partidas.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PartidaDao {
+    @Query(value = "SELECT * FROM Partidas ORDER BY partidaId DESC")
+    fun ObserveAll(): Flow<List<PartidaEntity>>
+
+    @Query(value = "SELECT * FROM Partidas WHERE partidaId = :id")
+    suspend fun getById(id: Int?): PartidaEntity?
+
+    @Upsert
+    suspend fun upsert(entity: PartidaEntity)
+
+    @Delete
+    suspend fun delete(entity: PartidaEntity)
+
+    @Query(value = "DELETE FROM Partidas WHERE partidaId = :id")
+    suspend fun deleteById(id: Int)
+
+}

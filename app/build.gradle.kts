ae4cd54e57a7aead78074d8fb15e239df2412cde
Alma Maria <alma_maria@ucne.edu.do>
2025-09-13 15:11:53 -0400

    eliminando plugins, dependecias y librerias viejas que estaban comentadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,43 +51,6 @@ android {
     }
 }
 
-//dependencies {
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.compose.ui)
-//    implementation(libs.androidx.compose.ui.graphics)
-//    implementation(libs.androidx.compose.ui.tooling.preview)
-//    implementation(libs.androidx.compose.material3)
-//
-//    //Hilt
-//    implementation("com.google.dagger:hilt-android:2.51")
-//    ksp("com.google.dagger:hilt-compiler:2.51")
-//    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-//
-//    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
-//
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-//    debugImplementation(libs.androidx.compose.ui.tooling)
-//    debugImplementation(libs.androidx.compose.ui.test.manifest)
-//
-//    // Navigation
-//    implementation(libs.androidx.navigation.compose)
-//    implementation(libs.kotlinx.serialization.json)
-//
-//    // Room
-//    implementation("androidx.room:room-runtime:2.6.1")
-//    implementation(libs.androidx.junit.ktx)
-//    implementation(libs.androidx.activity.ktx)
-//    annotationProcessor("androidx.room:room-compiler:2.6.1")
-//    ksp("androidx.room:room-compiler:2.6.1")
-//}
-
 dependencies {
 
     //imagenes
@@ -104,7 +67,6 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
-
     implementation("androidx.compose.foundation:foundation:1.5.0")
 
     //optional
@@ -113,8 +75,6 @@ dependencies {
     implementation ("androidx.compose.material:material:1.4.0")
     implementation("androidx.compose.material:material:1.3.1") // Usa la última versión
     implementation("androidx.compose.material:material-icons-extended")
-
-
 
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51")

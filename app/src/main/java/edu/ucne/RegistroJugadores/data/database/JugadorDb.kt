b63fbc9a7dc44d79607c8c100815cccf4fa1854a
Alma Maria <alma_maria@ucne.edu.do>
2025-09-14 22:11:04 -0400

    agregando partida y cambiando la version de la bd

--- app/src/main/java/edu/ucne/RegistroJugadores/data/database/JugadorDb.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/database/JugadorDb.kt
@@ -4,14 +4,19 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.RegistroJugadores.data.jugadores.local.JugadorDao
 import edu.ucne.RegistroJugadores.data.jugadores.local.JugadorEntity
+import edu.ucne.RegistroJugadores.data.partidas.local.PartidaDao
+import edu.ucne.RegistroJugadores.data.partidas.local.PartidaEntity
 
 @Database(
     entities = [
-        JugadorEntity::class
+        JugadorEntity::class,
+        PartidaEntity::class
     ],
-    version = 3,
+
+    version = 4,
     exportSchema = false
 )
 abstract class JugadorDb : RoomDatabase(){
     abstract fun jugadorDao() : JugadorDao
+    abstract fun partidaDao() : PartidaDao
 }

0a5ef00d7721bbabbcd68071cd91005924596c0e
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 04:06:48 -0400

    pasando el id

--- app/src/main/java/edu/ucne/RegistroJugadores/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/remote/RemoteDataSource.kt
@@ -6,6 +6,6 @@ import javax.inject.Inject
 class RemoteDataSource @Inject constructor(
     private val ticTacToeApi: TicTacToeApi
 ){
-    suspend fun getMovimiento(id: Int) = ticTacToeApi.getMovimiento()
+    suspend fun getMovimiento(id: Int) = ticTacToeApi.getMovimiento(id)
     suspend fun saveMovimientos(movientosDto: MovientosDto) = ticTacToeApi.saveMovimientos(movientosDto)
 }

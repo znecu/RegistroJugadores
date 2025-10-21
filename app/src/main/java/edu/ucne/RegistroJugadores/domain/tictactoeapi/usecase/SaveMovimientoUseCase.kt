5d06454e784a587905359c3a7dc406667ed2d428
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 03:35:57 -0400

    corrección de falta ortográfica

--- app/src/main/java/edu/ucne/RegistroJugadores/domain/tictactoeapi/usecase/SaveMovimientoUseCase.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/tictactoeapi/usecase/SaveMovimientoUseCase.kt
@@ -4,7 +4,7 @@ import edu.ucne.RegistroJugadores.domain.tictactoeapi.model.Movimiento
 import edu.ucne.RegistroJugadores.domain.tictactoeapi.repository.MovimientosRepository
 import javax.inject.Inject
 
-class SaveMovimientoUseCasa @Inject constructor(
+class SaveMovimientoUseCase @Inject constructor(
     private val repository: MovimientosRepository
 ) {
     suspend operator fun invoke(movimiento: Movimiento) = repository.saveMovimiento(movimiento)

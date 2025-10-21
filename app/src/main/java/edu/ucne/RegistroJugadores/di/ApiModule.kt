4d06887e10d704167819f5134add9dc2df0a3455
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 01:35:21 -0400

    creé el ApiModule.kt

---
+++ app/src/main/java/edu/ucne/RegistroJugadores/di/ApiModule.kt
@@ -0,0 +1,36 @@
+package edu.ucne.RegistroJugadores.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.RegistroJugadores.data.remote.TicTacToecApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/swagger/v1/swagger.json"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideTicketingApi(moshi: Moshi): TicTacToecApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TicTacToecApi::class.java)
+    }
+
+}

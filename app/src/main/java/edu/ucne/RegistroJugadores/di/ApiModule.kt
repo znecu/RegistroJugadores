f1a03f45029b5f1aa691dc65424420aa72a31f8d
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 04:08:28 -0400

    repositorymodule

--- app/src/main/java/edu/ucne/RegistroJugadores/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/di/ApiModule.kt
@@ -2,11 +2,16 @@ package edu.ucne.RegistroJugadores.di
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.RegistroJugadores.data.remote.TicTacToeApi
+import edu.ucne.RegistroJugadores.data.remote.repository.MovimientosRepositoryImpl
+import edu.ucne.RegistroJugadores.domain.tictactoeapi.repository.MovimientosRepository
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -14,7 +19,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
-    const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net"
+
+    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/"
 
     @Provides
     @Singleton
@@ -25,12 +31,34 @@ object ApiModule {
 
     @Provides
     @Singleton
-    fun provideTicTacToeApi(moshi: Moshi): TicTacToeApi {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
             .build()
-            .create(TicTacToeApi::class.java)
     }
 
+    @Provides
+    @Singleton
+    fun provideTicTacToeApi(moshi: Moshi, okHttpClient: OkHttpClient): TicTacToeApi =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+            .create(TicTacToeApi::class.java)
+
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMovimientosRepository(
+        movimientosRepositoryImpl: MovimientosRepositoryImpl
+    ): MovimientosRepository
 }

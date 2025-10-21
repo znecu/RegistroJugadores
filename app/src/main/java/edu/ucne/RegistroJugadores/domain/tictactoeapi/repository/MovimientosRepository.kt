9fdcc5679432c84eea1e8bc5aac58653080cfe88
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 03:35:40 -0400

    colocado el resource

--- app/src/main/java/edu/ucne/RegistroJugadores/domain/tictactoeapi/repository/MovimientosRepository.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/tictactoeapi/repository/MovimientosRepository.kt
@@ -1,5 +1,6 @@
 package edu.ucne.RegistroJugadores.domain.tictactoeapi.repository
 
+import com.google.api.ResourceReference
 import edu.ucne.RegistroJugadores.data.remote.Resource
 import edu.ucne.RegistroJugadores.domain.tictactoeapi.model.Movimiento
 import kotlinx.coroutines.flow.Flow
@@ -7,5 +8,5 @@ import kotlinx.coroutines.flow.Flow
 interface MovimientosRepository {
 
     fun getMovimiento(id: Int): Flow<Resource<Movimiento>>
-    suspend fun saveMovimiento(movimiento: Movimiento)
+    suspend fun saveMovimiento(movimiento: Movimiento): Resource<Unit>
 }

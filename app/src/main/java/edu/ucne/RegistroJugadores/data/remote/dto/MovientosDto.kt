83e829dc8fef1ee6d3d7d751eb93428a4378efca
Alma Maria <alma_maria@ucne.edu.do>
2025-10-21 01:37:04 -0400

    creé el Dto de movimientos

---
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/remote/dto/MovientosDto.kt
@@ -0,0 +1,8 @@
+package edu.ucne.RegistroJugadores.data.remote.dto
+
+data class MovientosDto(
+    val movimientoId: Int,
+    val jugador: String,
+    val posicionFila: Int,
+    val posicionColumna: Int,
+    )

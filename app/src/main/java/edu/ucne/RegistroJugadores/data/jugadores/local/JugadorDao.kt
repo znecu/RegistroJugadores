5e8cfcb95113c74854b33baa7b9ce2aa9d8bd59f
Alma Maria <alma_maria@ucne.edu.do>
2025-10-28 01:47:03 -0400

    agregando las nuevas funciones de Jugadores

--- app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/local/JugadorDao.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/local/JugadorDao.kt
@@ -4,15 +4,19 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import edu.ucne.RegistroJugadores.data.remote.Resource
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JugadorDao {
-    @Query(value = "SELECT * FROM Jugadores ORDER BY jugadorId DESC")
+    @Query(value = "SELECT * FROM Jugadores ORDER BY id DESC")
     fun ObserveAll(): Flow<List<JugadorEntity>>
 
-    @Query(value = "SELECT * FROM Jugadores WHERE jugadorId = :id")
+    @Query(value = "SELECT * FROM Jugadores WHERE id = :id")
     suspend fun getById(id: Int?): JugadorEntity?
+
+    @Query(value = "SELECT * FROM Jugadores WHERE id = :id")
+    suspend fun getById(id: String): JugadorEntity?
 
     @Upsert
     suspend fun upsert(entity: JugadorEntity)
@@ -20,9 +24,15 @@ interface JugadorDao {
     @Delete
     suspend fun delete(entity: JugadorEntity)
 
-    @Query(value = "DELETE FROM Jugadores WHERE jugadorId = :id")
+    @Query(value = "DELETE FROM Jugadores WHERE id = :id")
     suspend fun deleteById(id: Int)
 
-    @Query("SELECT EXISTS(SELECT 1 FROM Jugadores WHERE LOWER(TRIM(nombres)) = LOWER(TRIM(:nombre)) AND (:excludeId IS NULL OR jugadorId != :excludeId))")
+    @Query("SELECT EXISTS(SELECT 1 FROM Jugadores WHERE LOWER(TRIM(nombres)) = LOWER(TRIM(:nombre)) AND (:excludeId IS NULL OR id != :excludeId))")
     suspend fun existeNombre(nombre: String, excludeId: Int?): Boolean
+
+    @Query("SELECT * FROM Jugadores WHERE isPendingCreate = 1")
+    suspend fun getPendingCreateJugadores(): List<JugadorEntity>
+
+    @Query("DELETE FROM Jugadores WHERE id = :id")
+    suspend fun deleteById(id: String)
 }
--- app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/repository/JugadorRepository.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/repository/JugadorRepository.kt
@@ -1,5 +1,6 @@
 package edu.ucne.RegistroJugadores.domain.jugadores.repository
 
+import edu.ucne.RegistroJugadores.data.remote.Resource
 import edu.ucne.RegistroJugadores.domain.jugadores.model.Jugador
 import kotlinx.coroutines.flow.Flow
 
@@ -8,12 +9,23 @@ interface JugadorRepository {
 
     suspend fun getJugador(id: Int?) : Jugador?
 
-    suspend fun upsert(jugador: Jugador): Int
+//    suspend fun upsert(jugador: Jugador): Int
 
-    suspend fun delete(jugador: Jugador)
+    suspend fun delete(jugador: Jugador): Resource<Unit>
 
     suspend fun deleteById(id: Int)
 
     suspend fun existeNombre(nombre: String, excludeId: Int? = null): Boolean
+
+    //--
+    suspend fun getJugador(id: String): Jugador?
+    suspend fun createJugadorLocal(jugador: Jugador): Resource<Jugador>
+    suspend fun upsert(jugador: Jugador): Resource<Unit>
+    suspend fun delete(id: String): Resource<Unit>
+    suspend fun postPendingJugadores(): Resource<Unit>
+
+
+
+
 
 }

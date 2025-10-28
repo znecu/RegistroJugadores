d898c6e0c1836c630586df33ad25b515b8cf6e33
Alma Maria <alma_maria@ucne.edu.do>
2025-10-28 01:42:53 -0400

    adaptando el modelo jugadores

--- app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/model/Jugador.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/model/Jugador.kt
@@ -1,7 +1,11 @@
 package edu.ucne.RegistroJugadores.domain.jugadores.model
 
+import java.util.UUID
+
 data class Jugador(
-    val jugadorId: Int = 0,
-    val nombres: String = "",
-    val partidas: Int = 0
+    val id: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val nombres: String,
+    val email: String,
+    val isPendingCreate: Boolean = false
 )

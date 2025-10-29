3c88832fce908f01e2187727534e9eb689d870e1
Alma Maria <alma_maria@ucne.edu.do>
2025-10-29 12:37:28 -0400

    agregando el parametro de email en CrearJugador

--- app/src/main/java/edu/ucne/RegistroJugadores/presentation/jugadores/JugadorEvent.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/presentation/jugadores/JugadorEvent.kt
@@ -3,7 +3,7 @@ package edu.ucne.RegistroJugadores.presentation.jugadores
 import edu.ucne.RegistroJugadores.domain.jugadores.model.Jugador
 
 sealed interface JugadorEvent {
-    data class CrearJugador(val nombres: String) : JugadorEvent
+    data class CrearJugador(val nombres: String, val email: String) : JugadorEvent
     data class UpdateJugador(val jugador: Jugador) : JugadorEvent
     data class DeleteJugador(val id: String) : JugadorEvent
     object ShowCreateSheet : JugadorEvent

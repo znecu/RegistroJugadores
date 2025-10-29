e8e831ece5da1d7151509aaf4844cbb904a1d5fa
Alma Maria <alma_maria@ucne.edu.do>
2025-10-29 12:50:33 -0400

    moviendo el preview fuera de la clase y limpiando imports

--- app/src/main/java/edu/ucne/RegistroJugadores/MainActivity.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/MainActivity.kt
@@ -7,17 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.RegistroJugadores.presentation.componentes.TopBarComponent
+import edu.ucne.RegistroJugadores.presentation.navigation.TicTacToeNavHost
 import edu.ucne.RegistroJugadores.ui.theme.RegistroJugadoresTheme
-import edu.ucne.composedemo.presentation.navigation.TicTacToeNavHost
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -31,20 +28,19 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-
-    @Preview
-    @Composable
-    fun MainActivityPreview() {
-        RegistroJugadoresTheme {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
-                ) {
-                    MainActivity()
-                }
+@Preview
+@Composable
+fun MainActivityPreview() {
+    RegistroJugadoresTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                MainActivity()
             }
         }
     }

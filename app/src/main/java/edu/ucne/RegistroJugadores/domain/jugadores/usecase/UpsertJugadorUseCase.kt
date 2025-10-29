eae5583b2aad3821eb94c23305fe082128d80125
Alma Maria <alma_maria@ucne.edu.do>
2025-10-29 12:39:43 -0400

    corrigiendo e implementando validaciones faltantes

--- app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/usecase/UpsertJugadorUseCase.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/usecase/UpsertJugadorUseCase.kt
@@ -8,5 +8,17 @@ import javax.inject.Inject
 class UpsertJugadorUseCase @Inject constructor(
     private val repository: JugadorRepository
 ) {
-    suspend operator fun invoke(jugador: Jugador): Resource<Unit> = repository.upsert(jugador)
+    suspend operator fun invoke(jugador: Jugador): Resource<Unit> {
+        val nombreResult = validateNombres(jugador.nombres)
+        val emailResult = validateEmail(jugador.email)
+
+        if (!nombreResult.isValid) {
+            return Resource.Error(IllegalArgumentException(nombreResult.error).toString())
+        }
+        if (!emailResult.isValid) {
+            return Resource.Error(IllegalArgumentException(emailResult.error).toString())
+        }
+
+        return repository.upsert(jugador)
+    }
 }

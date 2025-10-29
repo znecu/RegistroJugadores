8c11ba86b4ff80b4e7359345e926f910b9eb29a5
Alma Maria <alma_maria@ucne.edu.do>
2025-10-28 23:26:57 -0400

    colocando el get

--- app/src/main/java/edu/ucne/RegistroJugadores/RegistroJugadoresApp.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/RegistroJugadoresApp.kt
@@ -1,8 +1,24 @@
 package edu.ucne.RegistroJugadores
 
 import android.app.Application
+import android.content.Context
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
+import edu.ucne.RegistroJugadores.data.remote.worker.MyWorkerFactory
+import edu.ucne.RegistroJugadores.data.remote.worker.SyncWorker
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RegistroJugadoresApp : Application() {}
+class RegistroJugadoresApp() : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
 

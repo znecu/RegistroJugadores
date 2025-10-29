43804b334fe632dc44b5b61ca091d7e6717fd3c3
Alma Maria <alma_maria@ucne.edu.do>
2025-10-29 12:46:39 -0400

    renombrando funciones

--- app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/local/JugadorDao.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/data/jugadores/local/JugadorDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
-import edu.ucne.RegistroJugadores.data.remote.Resource
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -27,12 +26,12 @@ interface JugadorDao {
     @Query(value = "DELETE FROM Jugadores WHERE id = :id")
     suspend fun deleteById(id: Int)
 
-    @Query("SELECT EXISTS(SELECT 1 FROM Jugadores WHERE LOWER(TRIM(nombres)) = LOWER(TRIM(:nombre)) AND (:excludeId IS NULL OR id != :excludeId))")
-    suspend fun existeNombre(nombre: String, excludeId: Int?): Boolean
+    @Query(value = "DELETE FROM Jugadores WHERE id = :id")
+    suspend fun deleteById(id: String)
+
+    @Query("SELECT EXISTS(SELECT 1 FROM Jugadores WHERE LOWER(TRIM(nombres)) = LOWER(TRIM(:nombre)) AND (:idJugadorActual IS NULL OR id != :idJugadorActual))")
+    suspend fun existeNombre(nombre: String, idJugadorActual: Int?): Boolean
 
     @Query("SELECT * FROM Jugadores WHERE isPendingCreate = 1")
     suspend fun getPendingCreateJugadores(): List<JugadorEntity>
-
-    @Query("DELETE FROM Jugadores WHERE id = :id")
-    suspend fun deleteById(id: String)
 }
--- app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/repository/JugadorRepository.kt
+++ app/src/main/java/edu/ucne/RegistroJugadores/domain/jugadores/repository/JugadorRepository.kt
@@ -5,27 +5,22 @@ import edu.ucne.RegistroJugadores.domain.jugadores.model.Jugador
 import kotlinx.coroutines.flow.Flow
 
 interface JugadorRepository {
-    fun observeJudador(): Flow<List<Jugador>>
 
-    suspend fun getJugador(id: Int?) : Jugador?
+    fun observeJugador(): Flow<List<Jugador>>
 
-//    suspend fun upsert(jugador: Jugador): Int
+    suspend fun getJugadorById(id: Int?): Jugador?
 
-    suspend fun delete(jugador: Jugador): Resource<Unit>
+    suspend fun getJugadorById(id: String): Jugador?
 
-    suspend fun deleteById(id: Int)
-
-    suspend fun existeNombre(nombre: String, excludeId: Int? = null): Boolean
-
-    //--
-    suspend fun getJugador(id: String): Jugador?
     suspend fun createJugadorLocal(jugador: Jugador): Resource<Jugador>
+
     suspend fun upsert(jugador: Jugador): Resource<Unit>
-    suspend fun delete(id: String): Resource<Unit>
+
+    suspend fun deleteJugador(jugador: Jugador): Resource<Unit>
+
+    suspend fun deleteJugadorById(id: String): Resource<Unit>
+
+    suspend fun jugadorExisteByNombre(nombre: String, idJugadorActual: Int?): Boolean
+
     suspend fun postPendingJugadores(): Resource<Unit>
-
-
-
-
-
 }
